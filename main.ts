184e27b238c5886a0e74b41965eb2c30ce6f505f
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2022-03-16 14:44:18 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+})
